2396fb72e9ec1adbbe4a2cad12822344121a5234
Amit kumar <kumaramit19100@gmail.com>
2022-04-27 19:50:28 +0530

    Commit 1

--- ApiTest_NBA_10/Program.cs
+++ ApiTest_NBA_10/Program.cs
@@ -13,43 +13,48 @@ namespace ApiTest_NBA_10
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Status { get; set; }
-        public int Height { get; set; }
-        public int Weight { get; set; }
         public string BirthDate { get; set; }
         public string PhotoUrl { get; set; }
         public string SportRadarPlayerID { get; set; }
         public string FanDuelName { get; set; }
+        public string YahooName { get; set; }
+        public string UsaTodayHeadshotUrl { get; set; }
+        public string UsaTodayHeadshotUpdated { get; set; }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            api().Wait();
+            Console.ReadKey();
         }
 
         public static async Task api()
         {
-            string key = "?Ocp-Apim-Subscription-Key=b2604730c1724b21ba76910924423bd7";
+            var Keys = "?Ocp-Apim-Subscription-Key=b2604730c1724b21ba76910924423bd7";
 
-            using(HttpClient client=new HttpClient())
+            using (HttpClient client=new HttpClient())
             {
                 client.BaseAddress = new Uri("https://api.sportsdata.io/v3/nba/scores/json/FreeAgents");
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription", "b2604730c1724b21ba76910924423bd7");
-                HttpResponseMessage message =await client.GetAsync(key);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "b2604730c1724b21ba76910924423bd7");
+               
+                HttpResponseMessage message =await client.GetAsync(Keys);
                 if (message.IsSuccessStatusCode)
                 {
                     var data = message.Content.ReadAsAsync<IEnumerable<Player>>().Result;
-                    foreach(var v in data)
+                    foreach (var v in data)
                     {
                         Console.WriteLine("Player Name : "+v.FirstName+" "+v.LastName);
                         Console.WriteLine("Player Id : "+v.PlayerID);
                         Console.WriteLine("Status : " + v.Status);
-                        Console.WriteLine("Height : " + v.Height);
-                        Console.WriteLine("Weight : " + v.Weight);
                         Console.WriteLine("Birth Date : " + v.BirthDate);
                         Console.WriteLine("Photo Url : " + v.PhotoUrl);
                         Console.WriteLine("Sport Radar Player ID : " + v.SportRadarPlayerID);
-                        Console.WriteLine("FanDuelName : " + v.FanDuelName);
+                        Console.WriteLine("Fan Duel Name : " + v.FanDuelName);
+                        Console.WriteLine("Yahoo Name : " + v.YahooName);
+                        Console.WriteLine("Usa Today Head Shot Url : " + v.UsaTodayHeadshotUrl);
+                        Console.WriteLine("Usa Today Head Shot Updated : " + v.UsaTodayHeadshotUpdated);
 
                     }
                 }
